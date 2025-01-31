9dd8dbd0fbbc322f718979076bd6e81652b6b151
Bach Davis <bachaarondavis@gmail.com>
2025-01-31 10:08:12 -0500

    added comments to components.py

--- components.py
+++ components.py
@@ -1,6 +1,7 @@
 from classes import Word, Thing
 
 
+# Things
 tSoweli = Thing("soweli") # animal
 tSinpin = Thing("sinpin") # wall
 tKiwen = Thing("kiwen") # rock
@@ -13,10 +14,12 @@ tSelo = Thing("selo") # fire
 tPipi = Thing("pipi") # bug
 tAle = Thing("ale") # all (everything; used for N. ala li statements)
 
-wLi = Word("li", 0)
-wEn = Word("en", 0)
-wAla = Word("ala", 0)
+# Particles
+wLi = Word("li", 0) # is; and (for predicates only)
+wEn = Word("en", 0) # and (for subjects only)
+wAla = Word("ala", 0) # not (for subjects and/or predicates)
 
+# Nouns
 wSoweli = Word("soweli", 1, tSoweli)
 wSinpin = Word("sinpin", 1, tSinpin)
 wKiwen = Word("kiwen", 1, tKiwen)
@@ -28,6 +31,7 @@ wIlo = Word("ilo", 1, tIlo)
 wSelo = Word("selo", 1, tSelo)
 wPipi = Word("pipi", 1, tPipi)
 
+# Adjectives
 wSina = Word("sina", 2) # you
 wPini = Word("pini", 2) # closed
 wOpen = Word("open", 2) # open
