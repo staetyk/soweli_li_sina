a4365173dd3dcd643a4d0fc3dbe86463b5d66593
Bach Davis <bachaarondavis@gmail.com>
2025-02-03 11:44:31 -0500

    added map init

---
+++ level_lib.py
@@ -0,0 +1,22 @@
+from components import *
+from csv import reader
+
+
+width = 1
+height = 1
+map = {0: []}
+
+def init(level: int):
+    global width
+    global height
+    global map
+    with open("levels.lvl{level}.csv") as file:
+        lines = reader(file)
+        i = -1
+        for x in lines:
+            if i == -1:
+                width, height = int(x[0]), int(x[1])
+                map = dict.fromkeys(range(width * height), [])
+            elif x[0] != "0":
+                map.update({i : [eval(y) for y in x]})
+            i += 1
