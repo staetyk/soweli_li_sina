00cdb9556c7981ef664737aa0e011e9373e608f5
Bach Davis <bachaarondavis@gmail.com>
2025-02-19 16:34:29 +0000

    added logo.png

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ pygame.init()
 
 screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
 pygame.display.set_caption("soweli li sina")
-# pygame.display.set_icon(pygame.image.load("images/logo.png"))
+pygame.display.set_icon(pygame.image.load("images/logo.png"))
