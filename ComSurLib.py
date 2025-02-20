1cd7ede4af12b3d3571a70fa6488029bd6ffa851
Bach Davis <bachaarondavis@gmail.com>
2025-02-20 12:06:28 +0000

    Updated TODO.txt

--- ComSurLib.py
+++ ComSurLib.py
@@ -5,7 +5,8 @@ from csv import reader
 style = {}
 with open("style.csv", "r") as file:
     for row in reader(file):
-        if row[1] == "n": style.update({row[0] : int(row[2])})
+        if row[2] == "": style.update({row[0] : None})
+        elif row[1] == "n": style.update({row[0] : int(row[2])})
         elif row[1] == "c": style.update({row[0] : int(row[2], 16)})
         elif row[1] == "p": style.update({row[0] : eval(row[2])})
         elif row[1] == "s": style.update({row[0] : row[2]})
