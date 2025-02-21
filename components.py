ff56e84ab2afa9e68e2b349908b80f47b542a411
Bach Davis <bachaarondavis@gmail.com>
2025-02-21 16:03:36 +0000

    Updated components.py

--- components.py
+++ components.py
@@ -8,33 +8,33 @@ tKiwen = Thing("kiwen", sprite = "Things/tKiwen") # rock
 tKon = Thing("kon") # air (empty cell)
 tLen = Thing("len", sprite = "Things/tLen") # flag
 tSeli = Thing("seli", sprite = "Things/tSeli") # fire
-tLupa = Thing("lupa") # door
-tIlo = Thing("ilo", facing = 2) # key
-tSelo = Thing("selo") # border
-tPipi = Thing("pipi") # bug
+tLupa = Thing("lupa", sprite = "Things/tLupa") # door
+tIlo = Thing("ilo", sprite = "Things/tIlo", facing = 2) # key
+tSelo = Thing("selo", sprite = "Things/tSelo") # border
+tPipi = Thing("pipi", sprite = "Things/tPipi") # bug
 
 # Particles
-wLi = Word("li", 0) # is; and (for predicates only)
-wEn = Word("en", 0) # and (for subjects only)
-wAla = Word("ala", 0) # not (for subjects and/or predicates)
+wLi = Word("li", 0, sprite = "Words/wLi") # is; and (for predicates only)
+wEn = Word("en", 0, sprite = "Words/wEn") # and (for subjects only)
+wAla = Word("ala", 0, sprite = "Words/wAla") # not (for subjects and/or predicates)
 
 # Nouns
-wSoweli = Word("soweli", 1, tSoweli)
-wSinpin = Word("sinpin", 1, tSinpin)
-wKiwen = Word("kiwen", 1, tKiwen)
-wKon = Word("kon", 1, tKon)
-wLen = Word("len", 1, tLen)
-wSeli = Word("seli", 1, tSeli)
-wLupa = Word("lupa", 1, tLupa)
-wIlo = Word("ilo", 1, tIlo)
-wSelo = Word("selo", 1, tSelo)
-wPipi = Word("pipi", 1, tPipi)
+wSoweli = Word("soweli", 1, tSoweli, sprite = "Words/wSoweli")
+wSinpin = Word("sinpin", 1, tSinpin, sprite = "Words/wSinpin")
+wKiwen = Word("kiwen", 1, tKiwen, sprite = "Words/wKiwen")
+wKon = Word("kon", 1, tKon, sprite = "Words/wKon")
+wLen = Word("len", 1, tLen, sprite = "Words/wLen")
+wSeli = Word("seli", 1, tSeli, sprite = "Words/wSeli")
+wLupa = Word("lupa", 1, tLupa, sprite = "Words/wLupa")
+wIlo = Word("ilo", 1, tIlo, sprite = "Words/wIlo")
+wSelo = Word("selo", 1, tSelo, sprite = "Words/wSelo")
+wPipi = Word("pipi", 1, tPipi, sprite = "Words/wPipi")
 
 # Adjectives
-wSina = Word("sina", 2) # you
-wPini = Word("pini", 2) # closed
-wOpen = Word("open", 2) # open
-wAwen = Word("awen", 2) # stop
-wTawa = Word("tawa", 2) # push
-wPona = Word("pona", 2) # win
-wMoli = Word("moli", 2) # kill
+wSina = Word("sina", 2, sprite = "Words/wSina") # you
+wPini = Word("pini", 2, "Words/wPini") # closed
+wOpen = Word("open", 2, "Words/wOpen") # open
+wAwen = Word("awen", 2, sprite = "Words/wAwen") # stop
+wTawa = Word("tawa", 2, sprite = "Words/wTawa") # push
+wPona = Word("pona", 2, sprite = "Words/wPona") # win
+wMoli = Word("moli", 2, sprite = "Words/wMoli") # kill
