3afe60ea0ec3e76af25230e42a08d27838483925
Bach Davis (School) <bdavis@student.newmarkeducation.com>
2025-04-09 17:16:12 +0000

    added nnavigatiion

--- lib/sel_scene.py
+++ lib/sel_scene.py
@@ -12,13 +12,16 @@ def check(i: int) -> bool:
     else: return True
 
 
-num, cursed = 1, 0
+num, curse = 1, [0, 0, 0]
 
 def init():
-    global num, cursed
+    global num, curse
     num = 0
     while check(num): num += 1
-    cursed = 0
+    curse = [0, 0, 0]
+
+
+cursed = lambda : curse[0] * ComSurLib.style["sel_num_x"] * ComSurLib.style["sel_num_y"] + curse[1] * ComSurLib.style["sel_num_x"] + curse[2]
 
 
 def frame(dim: tuple[int, int], preSc: float, key: int) -> tuple[float, Optional[pygame.Surface]]:
@@ -28,8 +31,25 @@ def frame(dim: tuple[int, int], preSc: float, key: int) -> tuple[float, Optional
     if key == 0:
         try: pygame.mixer.Sound("sounds/click.mp3").play()
         except: pass
-        return (cursed / 100, None)
-    elif key == 6: return (4, None)        
+        return (cursed() / 100, None)
+    elif key == 6: return (4, None) 
+    elif key == 1:
+        if curse[2] > 0: curse[2] -= 1
+    elif key == 3:
+        if cursed() + ComSurLib.style["sel_num_x"] < num: pass
+        elif curse[2] < ComSurLib.style["sel_num_y"]: curse[2] += 1
+    elif key == 2:
+        if cursed() + 1 >= num: pass
+        elif curse[1] < ComSurLib.style["sel_num_x"]: curse[1] += 1
+        elif cursed() + ComSurLib.style["sel_num_x"] * ComSurLib.style["sel_num_y"] - curse[1] >= num: pass
+        elif curse[0] < ceil(num / (ComSurLib.style["sel_num_x"] * ComSurLib.style["sel_num_y"])):
+            curse[1] = 0
+            curse[0] += 1
+    elif key == 4:
+        if curse[1] > 0: curse[1] -= 1
+        elif curse[0] > 0:
+            curse[1] = ComSurLib.style["sel_num_x"]
+            curse[0] -= 1
 
     page = cursed // (ComSurLib.style["sel_num_x"] * ComSurLib.style["sel_num_y"])
 
@@ -45,7 +65,7 @@ def frame(dim: tuple[int, int], preSc: float, key: int) -> tuple[float, Optional
             
             cell = pygame.Rect(dim[0] * j * ComSurLib.style["sel_lvl_siz_x"] // ComSurLib.style["sel_num_x"], dim[1] * i * ComSurLib.style["sel_lvl_siz_y"] // ComSurLib.style["sel_num_y"], dim[0] * ComSurLib.style["sel_lvl_siz_x"] // ComSurLib.style["sel_num_x"], dim[1] * ComSurLib.style["sel_lvl_siz_y"] // ComSurLib.style["sel_num_y"])
             
-            level = ComSurLib.button(str(n).zfill(2), ComSurLib.lang(), ComSurLib.style["sel_lvl_txt_font"], ComSurLib.style["sel_lvl_txt_var"], ComSurLib.style[f"sel_lvl_{this}_txt"], ComSurLib.style["sel_lvl_txt_pad_x"], ComSurLib.style["sel_lvl_txt_pad_y"], ComSurLib.style[f"sel_lvl_{this}_fill"], ComSurLib.style[f"sel_lvl_{this}_out"], ComSurLib.style["sel_lvl_wid"], ComSurLib.style["sel_lvl_siz_x"] * cell.width * (1 - 2 * (ComSurLib.style["sel_cur_wid_y"] + ComSurLib.style["sel_cur_pad_x"])), ComSurLib.style["sel_lvl_siz_y"] * cell.height * (1 - 2 * (ComSurLib.style["sel_cur_wid_x"] + ComSurLib.style["sel_cur_pad_y"])), ComSurLib.style["sel_lvl_rad"], n == cursed, ComSurLib.style["sel_cur_col"], ComSurLib.style["sel_cur_pad_x"], ComSurLib.style["sel_cur_pad_y"], ComSurLib.style["sel_cur_len_x"], ComSurLib.style["sel_cur_len_y"], ComSurLib.style["sel_cur_wid_x"], ComSurLib.style["sel_cur_wid_y"], True, True, ComSurLib.style["sel_bg"])
+            level = ComSurLib.button(str(n).zfill(2), ComSurLib.lang(), ComSurLib.style["sel_lvl_txt_font"], ComSurLib.style["sel_lvl_txt_var"], ComSurLib.style[f"sel_lvl_{this}_txt"], ComSurLib.style["sel_lvl_txt_pad_x"], ComSurLib.style["sel_lvl_txt_pad_y"], ComSurLib.style[f"sel_lvl_{this}_fill"], ComSurLib.style[f"sel_lvl_{this}_out"], ComSurLib.style["sel_lvl_wid"], ComSurLib.style["sel_lvl_siz_x"] * cell.width * (1 - 2 * (ComSurLib.style["sel_cur_wid_y"] + ComSurLib.style["sel_cur_pad_x"])), ComSurLib.style["sel_lvl_siz_y"] * cell.height * (1 - 2 * (ComSurLib.style["sel_cur_wid_x"] + ComSurLib.style["sel_cur_pad_y"])), ComSurLib.style["sel_lvl_rad"], n == cursed(), ComSurLib.style["sel_cur_col"], ComSurLib.style["sel_cur_pad_x"], ComSurLib.style["sel_cur_pad_y"], ComSurLib.style["sel_cur_len_x"], ComSurLib.style["sel_cur_len_y"], ComSurLib.style["sel_cur_wid_x"], ComSurLib.style["sel_cur_wid_y"], True, True, ComSurLib.style["sel_bg"])
 
             padded = ((cell.width - level.get_width()) / 2, (cell.height - level.get_height()) / 2)
             base.blit(level, (cell.x + padded[0], cell.y + padded[1]))
